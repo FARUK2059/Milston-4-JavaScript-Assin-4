35874fb41d057bf3f1a65fdd9c566b40184e3b77
OMAR FARUK <omarfaruk34552@gmail.com>
2024-02-07 22:35:37 +0600

    Assinment-4 Tas- 1

---
+++ m23-Assin-Task-1.js
@@ -0,0 +1,36 @@
+                            /******** > Assinment Task 1 < *******/ 
+
+
+/**
+ ********** ি ড়ি য়াখানায় প্রতি টি কে ট বি ক্রি হয় ১২০ টাকায় । আর ব্যয় এর মধ্যে প্রতি দি ন ১ জন দার োয়ান কে দি তে হয় ৫০০
+টাকা এবং ৮ জন স্টাফ কে লাঞ্চ করান োর জন্যে খরচ হয় জনপ্রতি ৫০ টাকা করে । 
+ **/ 
+
+
+function calculateMoney(ticketSale){
+    // console.log(ticketSale);
+
+    if (ticketSale < 0){
+        return 'Invalid Number';
+    }
+
+    const ticketPrice = 120;
+    const gardCost = 500;
+    const stafeCost = 8 * 50 ;
+
+    const income = ticketPrice * ticketSale;
+    const totalCost = gardCost + stafeCost;
+
+    const dailyIncom = income - totalCost;
+
+    return dailyIncom;
+}
+
+// const dailyIncom = calculateMoney(10);
+// const dailyIncom = calculateMoney(1055);
+// const dailyIncom = calculateMoney(93);
+// const dailyIncom = calculateMoney(-130);
+// const dailyIncom = calculateMoney(-25);
+const dailyIncom = calculateMoney(25);
+
+console.log('Mr. Baker daily income is :', dailyIncom);
