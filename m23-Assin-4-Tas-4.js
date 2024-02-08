053acbfc6290fb182072a7d268fac0feaa3d9a21
OMAR FARUK <omarfaruk34552@gmail.com>
2024-02-08 16:36:38 +0600

    Make A Great Password

---
+++ m23-Assin-4-Tas-4.js
@@ -0,0 +1,31 @@
+                            /******** > Assinment Task 4 < *******/ 
+
+
+/**
+ *** Problem 04 : Make A Great Password
+ 
+ ********** ফাংশন টি একটা object ইনপুট হি সে বে নি বে । যে খানে শুধুতি ন টি প্র োপার্টি থাকবে ।
+            { name: “kolimuddin” , birthYear: 1999 , siteName: “google” }
+ **/ 
+
+const allobj = { name: "KolimUddin", birthYear : 2000, siteName: "Google" } ;
+
+function password(obj) {
+    // console.log(obj);
+
+    const {name, birthYear, siteName} = obj;
+    if (!name || !birthYear || !siteName) {
+        return "invalid";
+    }
+
+    if (typeof birthYear !== 'number' || birthYear.toString().length !== 4) {
+        return "invalid";
+    }
+
+    const outputPassword = siteName + '#' + name + '@' + birthYear;
+    return outputPassword;
+
+}
+
+const passwordDitails = password(allobj);
+console.log('The save password is :', passwordDitails);
